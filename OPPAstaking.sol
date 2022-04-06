d44db1dd7a3764d29c901ba8103e117be1ba633c
"Joshua Fuentes" <"joshuarpf@gmail.com">
2022-04-06 23:22:11 +0800

    Added interface for BEP20 and balance fetching

--- OPPAstaking.sol
+++ OPPAstaking.sol
@@ -2,24 +2,27 @@
 
 pragma solidity ^0.8.4;
 
-// interface IBEP20 {
-//     function totalSupply() external view returns (uint256);
-//     function decimals() external view returns (uint8);
-//     function symbol() external view returns (string memory);
-//     function name() external view returns (string memory);
-//     function getOwner() external view returns (address);
-//     function balanceOf(address account) external view returns (uint256);
-//     function transfer(address recipient, uint256 amount) external returns (bool);
-//     function allowance(address _owner, address spender) external view returns (uint256);
-//     function approve(address spender, uint256 amount) external returns (bool);
-//     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-//     event Transfer(address indexed from, address indexed to, uint256 value);
-//     event Approval(address indexed owner, address indexed spender, uint256 value);
-// }
+interface IBEP20 {
+    function totalSupply() external view returns (uint256);
+    function decimals() external view returns (uint8);
+    function symbol() external view returns (string memory);
+    function name() external view returns (string memory);
+    function getOwner() external view returns (address);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address _owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
 
 contract OPPA_staking {
 
     address private _deployer;
+
+    address _staking_token = 0x431AcF08757484eE54051978143b0a61268e1c7f;
+
 
     constructor() {
         _deployer = msg.sender;
@@ -27,5 +30,9 @@ contract OPPA_staking {
 
     function getDeployer() public view returns(address) {
         return _deployer;
+    }
+
+    function getTokenBalance() public view returns(uint256) {
+        return IBEP20(_staking_token).balanceOf(msg.sender);
     }
 }
