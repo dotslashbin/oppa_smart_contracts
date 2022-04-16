f8b35b3aafa55ad668846c3736d1cf23a842c555
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2022-04-16 22:17:37 +0800

    Refactored for uniformity

--- Admin.sol
+++ Admin.sol
@@ -7,24 +7,24 @@ import "./Interfaces.sol";
 
 contract Admin {
 
-    bool public _isActive; 
-    uint internal _percentageOfRewards;
-    uint internal _rewardsFrequencyInMinutes;
-    Authorizer _contractAuthorizer; 
+    bool public _is_active; 
+    uint internal _percentage_of_rewards;
+    uint internal _rewards_frequency_in_minutes;
+    Authorizer _contract_authorizer; 
     address private _staking_token; // TODO: repalce this with the correct OPPA address
 
     constructor() {
-        _contractAuthorizer = new Authorizer(msg.sender);
+        _contract_authorizer = new Authorizer(msg.sender);
     }
 
     // Modifiers
     modifier isAuthorized() {
-        require(_contractAuthorizer.getDeployer() == msg.sender, "You do not have the authority for that."); 
+        require(_contract_authorizer.getDeployer() == msg.sender, "You do not have the authority for that."); 
         _;
     }
 
     function GetAdminAddress() isAuthorized public view returns(address) {
-        return _contractAuthorizer.getDeployer();
+        return _contract_authorizer.getDeployer();
     }
 
     /**
@@ -39,28 +39,28 @@ contract Admin {
     }
 
     function IsStakingActive() internal view returns(bool) {
-        return _isActive;
+        return _is_active;
     }
 
     /**
      * Checks if the staking is active
      */ 
     function Pause() isAuthorized public {
-        _isActive = false;
+        _is_active = false;
     }
 
     /**
      * Sets reward frequency
      */
     function SetRewardsFrequency(uint _value) isAuthorized public {
-        _rewardsFrequencyInMinutes = _value;
+        _rewards_frequency_in_minutes = _value;
     }
 
     /**
      * Set the percentage of reward 
      */ 
     function SetRewardsPercentage(uint _value) isAuthorized public {
-        _percentageOfRewards = _value; 
+        _percentage_of_rewards = _value; 
     }
 
     function SetStakingTokenAddress(address input) isAuthorized public {
@@ -71,12 +71,17 @@ contract Admin {
      * Sets to start / unpause the staking
      */ 
     function UnPause() isAuthorized public {
-        _isActive = true; 
+        _is_active = true; 
     }
 
-    // function WidthrawToWallet(address _wallet, uint256 _amount) public returns(bool success) {
-    //     require(GetAvailableStakingBalance() > 0, "There are no more toekns in the contract."); 
-    //     // _wallet += amount
-    //     return true; 
-    // }
+    function WidthrawToWallet(address _wallet, uint256 _amount) isAuthorized public returns (bool success) {
+        require(_amount > 0, "Widthrawal needs a value.");
+        require(GetAvailableStakingBalance() > _amount, "This contract does not have enough balance to send");
+
+        bool result = IBEP20(GetStakingTokenAddress()).approve(address(this), _amount); 
+        require(result == true, "Approval failed"); 
+        IBEP20(GetStakingTokenAddress()).transferFrom(address(this), _wallet, _amount);
+
+        return true; 
+    }
 }
